a7fdb8e032c93b81493191a73d23f3ea82eb071f
Marcio Martinez <marciomartinez500@gmail.com>
2019-03-02 20:58:51 -0600

    Primer Commit App

---
+++ ModificadorResoluciones/Form1.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.FlpResoluciones = New System.Windows.Forms.FlowLayoutPanel()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.CboFormato = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PbImagen = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TxtNombre = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PbImagen, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'FlpResoluciones
+        '
+        Me.FlpResoluciones.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlpResoluciones.AutoScroll = True
+        Me.FlpResoluciones.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.FlpResoluciones.Location = New System.Drawing.Point(191, 9)
+        Me.FlpResoluciones.Name = "FlpResoluciones"
+        Me.FlpResoluciones.Size = New System.Drawing.Size(380, 443)
+        Me.FlpResoluciones.TabIndex = 3
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(191, 458)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(380, 32)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "CAMBIAR RESOLUCIÓN"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.TxtNombre)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.CboFormato)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.PbImagen)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(185, 499)
+        Me.Panel1.TabIndex = 8
+        '
+        'CboFormato
+        '
+        Me.CboFormato.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.CboFormato.FormattingEnabled = True
+        Me.CboFormato.Items.AddRange(New Object() {"PNG", "JPEG", "ICON", "GIF", "BMP"})
+        Me.CboFormato.Location = New System.Drawing.Point(7, 344)
+        Me.CboFormato.Name = "CboFormato"
+        Me.CboFormato.Size = New System.Drawing.Size(173, 27)
+        Me.CboFormato.TabIndex = 11
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(7, 232)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(173, 32)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "SELECCIONAR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PbImagen
+        '
+        Me.PbImagen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PbImagen.Location = New System.Drawing.Point(7, 56)
+        Me.PbImagen.Name = "PbImagen"
+        Me.PbImagen.Size = New System.Drawing.Size(173, 170)
+        Me.PbImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PbImagen.TabIndex = 9
+        Me.PbImagen.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(4, 7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(151, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Cantidad de Resoluciones"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(7, 23)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(173, 27)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(4, 328)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Formato"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(4, 277)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(136, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Nombre Nueva Imagen"
+        '
+        'TxtNombre
+        '
+        Me.TxtNombre.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtNombre.Location = New System.Drawing.Point(7, 293)
+        Me.TxtNombre.Name = "TxtNombre"
+        Me.TxtNombre.Size = New System.Drawing.Size(173, 27)
+        Me.TxtNombre.TabIndex = 13
+        Me.TxtNombre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(578, 499)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.FlpResoluciones)
+        Me.MinimumSize = New System.Drawing.Size(594, 338)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Modificador Resoluciones 2019"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PbImagen, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents FlpResoluciones As FlowLayoutPanel
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PbImagen As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents CboFormato As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TxtNombre As TextBox
+End Class
---
+++ ModificadorResoluciones/Form1.vb
@@ -0,0 +1,199 @@
+﻿' DESARROLLADO POR MARCIO FRANCISCO MARTINEZ OCHOA
+' CORREO: marciomartinez500@gmail.com
+Public Class Form1
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        e.Handled = Not (Char.IsDigit(e.KeyChar) Or Asc(e.KeyChar) = 8)
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim cantidad As Integer = 0
+        Dim controles As New List(Of Control)
+
+        If CType(sender, TextBox).Text.Trim <> "" Then
+            cantidad = CInt(CType(sender, TextBox).Text)
+        End If
+
+        For Each control As Control In FlpResoluciones.Controls
+            controles.Add(control)
+        Next
+
+        For Each control As Control In controles
+            FlpResoluciones.Controls.Remove(control)
+            control.Dispose()
+        Next
+
+        For i As Integer = 0 To cantidad - 1
+
+            Dim _TextBox1, _TextBox2 As New TextBox
+            Dim _GroupBox As New GroupBox
+
+            _TextBox1.Location = New Point(14, 15)
+            _TextBox1.Name = $"Txt1Resolucion{i + 1}"
+            _TextBox1.Size = New Size(71, 20)
+            _TextBox1.Text = "Ancho"
+            _TextBox1.ForeColor = Color.Gray
+
+            _TextBox2.Location = New System.Drawing.Point(91, 15)
+            _TextBox2.Name = $"Txt2Resolucion{i + 1}"
+            _TextBox2.Size = New Size(71, 20)
+            _TextBox2.Text = "Alto"
+            _TextBox2.ForeColor = Color.Gray
+
+
+            AddHandler _TextBox1.KeyPress, AddressOf Evento_KeyPress
+            AddHandler _TextBox2.KeyPress, AddressOf Evento_KeyPress
+            AddHandler _TextBox1.TextChanged, AddressOf Evento_TextChanged
+            AddHandler _TextBox2.TextChanged, AddressOf Evento_TextChanged
+            AddHandler _TextBox1.GotFocus, AddressOf Evento_GotFocus
+            AddHandler _TextBox2.GotFocus, AddressOf Evento2_GotFocus
+            AddHandler _TextBox1.LostFocus, AddressOf Evento_LostFocus
+            AddHandler _TextBox2.LostFocus, AddressOf Evento2_LostFocus
+
+            _GroupBox.Controls.Add(_TextBox1)
+            _GroupBox.Controls.Add(_TextBox2)
+            _GroupBox.Location = New Point(3, 3)
+            _GroupBox.Name = $"Resolucion{i + 1}"
+            _GroupBox.Size = New Size(173, 45)
+            _GroupBox.Font = New Font("Tahoma", 8.25!, FontStyle.Bold, GraphicsUnit.Point, CType(0, Byte))
+            _GroupBox.TabStop = False
+            _GroupBox.Text = $"Resolución #{i + 1}"
+
+            FlpResoluciones.Controls.Add(_GroupBox)
+
+        Next
+    End Sub
+
+    Private Sub Evento_KeyPress(sender As Object, e As KeyPressEventArgs)
+        e.Handled = Not (Char.IsDigit(e.KeyChar) Or Asc(e.KeyChar) = 8)
+    End Sub
+    Private Sub Evento_TextChanged(sender As Object, e As EventArgs)
+        If CType(sender, TextBox).Text.Trim = "Ancho" Or CType(sender, TextBox).Text.Trim = "Alto" Then
+            ErrorProvider1.SetError(CType(sender, TextBox), $"Ingrese el {CType(sender, TextBox).Text.Trim}")
+        Else
+            ErrorProvider1.SetError(CType(sender, TextBox), "")
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            OpenFileDialog1.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyPictures
+            OpenFileDialog1.Filter = "Todos los archivos (*.*)|*.*"
+            OpenFileDialog1.Title = "Seleccione la imagen"
+
+            If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+                PbImagen.Image = Image.FromFile(OpenFileDialog1.FileName)
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If PbImagen.Image Is Nothing Then
+            MessageBox.Show("Debe seleccionar una imagen", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+        If FlpResoluciones.Controls.Count = 0 Then
+            MessageBox.Show("Debe tener almenos una resolución", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+        If TxtNombre.Text.ToString.Trim = "" Then
+            MessageBox.Show("Ingrese el nombre de la nueva imagen", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+        If CboFormato.Text.ToString.Trim = "" Then
+            MessageBox.Show("Seleccione el formato de imagen", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        Dim i As Integer = 1
+        Dim sin_errores As Boolean = True
+        For Each GroupBox As Control In FlpResoluciones.Controls
+            Dim gb As GroupBox = TryCast(GroupBox, GroupBox)
+
+            If gb.Controls.Find($"Txt1Resolucion{i}", True).First().Text.Trim = "Ancho" Then
+                Dim txt As TextBox = DirectCast(gb.Controls.Find($"Txt1Resolucion{i}", True).First(), TextBox)
+                ErrorProvider1.SetError(txt, "Ingrese el Ancho")
+                sin_errores = False
+            End If
+            If gb.Controls.Find($"Txt2Resolucion{i}", True).First().Text.Trim = "Alto" Then
+                Dim txt As TextBox = DirectCast(gb.Controls.Find($"Txt2Resolucion{i}", True).First(), TextBox)
+                ErrorProvider1.SetError(txt, "Ingrese el Alto")
+                sin_errores = False
+            End If
+            i = i + 1
+        Next
+
+        i = 1
+        If sin_errores Then
+            SaveFileDialog1.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyPictures
+            SaveFileDialog1.Title = "Ingrese el nombre de la carpeta"
+
+            If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+                System.IO.Directory.CreateDirectory(SaveFileDialog1.FileName)
+
+                For Each GroupBox As Control In FlpResoluciones.Controls
+                    Dim gb As GroupBox = TryCast(GroupBox, GroupBox)
+                    Dim x1, x2 As Integer
+
+                    x1 = CInt(DirectCast(gb.Controls.Find($"Txt1Resolucion{i}", True).First(), TextBox).Text.Trim)
+                    x2 = CInt(DirectCast(gb.Controls.Find($"Txt2Resolucion{i}", True).First(), TextBox).Text.Trim)
+
+                    Using Bitmap As New Bitmap(PbImagen.Image, New Size(x1, x2))
+                        If CboFormato.SelectedItem.ToString = "PNG" Then
+                            Bitmap.Save(SaveFileDialog1.FileName + $"/{TxtNombre.Text}_{x1}_x_{x2}_{i}.png", Imaging.ImageFormat.Png)
+                        ElseIf CboFormato.SelectedItem.ToString = "JPEG" Then
+                            Bitmap.Save(SaveFileDialog1.FileName + $"/{TxtNombre.Text}_{x1}_x_{x2}_{i}.jpg", Imaging.ImageFormat.Jpeg)
+                        ElseIf CboFormato.SelectedItem.ToString = "ICON" Then
+                            Bitmap.Save(SaveFileDialog1.FileName + $"/{TxtNombre.Text}_{x1}_x_{x2}_{i}.ico", Imaging.ImageFormat.Icon)
+                        ElseIf CboFormato.SelectedItem.ToString = "GimagenIF" Then
+                            Bitmap.Save(SaveFileDialog1.FileName + $"/{TxtNombre.Text}_{x1}_x_{x2}_{i}.gif", Imaging.ImageFormat.Gif)
+                        ElseIf CboFormato.SelectedItem.ToString = "BMP" Then
+                            Bitmap.Save(SaveFileDialog1.FileName + $"/{TxtNombre.Text}_{x1}_x_{x2}_{i}.bmp", Imaging.ImageFormat.Bmp)
+                        End If
+                    End Using
+                    i = i + 1
+                Next
+                MessageBox.Show("Imagenes almacenadas con exito!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Application.Restart()
+            End If
+        End If
+    End Sub
+
+    Private Sub Evento_GotFocus(sender As Object, e As EventArgs)
+        If CType(sender, TextBox).Text.Trim = "Ancho" Then
+            CType(sender, TextBox).Text = ""
+            CType(sender, TextBox).ForeColor = Color.Black
+        Else
+            CType(sender, TextBox).ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub Evento_LostFocus(sender As Object, e As EventArgs)
+        If CType(sender, TextBox).Text.Trim = "" Then
+            CType(sender, TextBox).Text = "Ancho"
+            CType(sender, TextBox).ForeColor = Color.Gray
+        Else
+            CType(sender, TextBox).ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub Evento2_GotFocus(sender As Object, e As EventArgs)
+        If CType(sender, TextBox).Text.Trim = "Alto" Then
+            CType(sender, TextBox).Text = ""
+            CType(sender, TextBox).ForeColor = Color.Black
+        Else
+            CType(sender, TextBox).ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub Evento2_LostFocus(sender As Object, e As EventArgs)
+        If CType(sender, TextBox).Text.Trim = "" Then
+            CType(sender, TextBox).Text = "Alto"
+            CType(sender, TextBox).ForeColor = Color.Gray
+        Else
+            CType(sender, TextBox).ForeColor = Color.Black
+        End If
+    End Sub
+End Class
